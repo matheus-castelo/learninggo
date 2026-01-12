aa7c33c9f96a95c9629df3b45e07f3a8ca09800a
Matheus Castelo <matheuscastelo.tech@gmail.com>
2026-01-12 20:36:04 -0300

    tratamento de erros

---
+++ src/configuration/rest_err/rest.err.go
@@ -0,0 +1,72 @@
+package rest_err
+
+import "net/http"
+
+type RestErr struct {
+	Message string  `json:"message"`
+	Err string `json:"error"`
+	Code int64 `json:"code"`
+	Causes []Causes `json:"causes"`
+}
+
+type Causes struct {
+	Field string `json:"field"`
+	Message string `json:"message"`
+}
+
+// Algumas libs retornam/recebem erros por parametros. Criei uma interface para quando precisar mandar um erro por parametro, mandar um RestErr
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+
+func NewRestErr(message, err string, code int64, causes [] Causes) *RestErr{
+	return &RestErr{
+		Message: message,
+		Err: err,
+		Code: code,
+		Causes: causes,
+
+	}
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err: "bad_request",
+		Code: http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err: "bad_request",
+		Code: http.StatusBadRequest,
+		Causes: causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr{
+	return &RestErr{
+		Message: message,
+		Err: "internal_server_error",
+		Code: http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr{
+	return &RestErr{
+		Message: message,
+		Err: "not_found",
+		Code: http.StatusInternalServerError,
+	}
+}
+
+func NewForbidden(message string) *RestErr{
+	return &RestErr{
+		Message: message,
+		Err: "forbidden",
+		Code: http.StatusForbidden,
+	}
+}
