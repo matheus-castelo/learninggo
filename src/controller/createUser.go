eff3aa8deed5a97a66822a5d98aef20aba383795
Matheus Castelo <matheuscastelo.tech@gmail.com>
2026-01-13 10:33:43 -0300

    rota create user

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,12 +1,23 @@
 package controller
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/matheus-castelo/learninggo/src/configuration/rest_err" 
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/matheus-castelo/learninggo/src/configuration/rest_err"
+	"github.com/matheus-castelo/learninggo/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
-    
-    err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
-    c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		c.JSON(int(restErr.Code), restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+	c.JSON(http.StatusOK, userRequest)
 }
